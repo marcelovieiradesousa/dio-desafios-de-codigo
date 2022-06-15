2647ed5f816d5c36103f778ab39c95f05a6e7382
Elidiana Andrade <97471199+elidianaandrade@users.noreply.github.com>
2022-06-15 17:21:43 -0300

    Update resolucao-codigo.js

--- desafios-javascript/desafios-iniciais-javascript/desafio-04-quadrado-de-pares/resolucao-codigo.js
+++ desafios-javascript/desafios-iniciais-javascript/desafio-04-quadrado-de-pares/resolucao-codigo.js
@@ -3,9 +3,9 @@ let lines = gets().split('\n');
 var N = parseInt(lines.shift());
 
 for (let i = 1; i <= N; i++) {
-let quadrado = Math.pow(i, 2);
+ let quadrado = Math.pow(i, 2);
 
-	if (i % 2 === 0) 
-    console.log(`${i}^2 = ${quadrado}`);
+ if (i % 2 === 0) 
+ console.log(`${i}^2 = ${quadrado}`);
 }
 // RESOLUÇÃO QUADRADO DE PARES END
